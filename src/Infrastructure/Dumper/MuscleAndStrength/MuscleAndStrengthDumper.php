4544787ac755a544466fccab6ca0530437b0aeea
Pi-Bouf <pierre.bouffier05@gmail.com>
2022-07-12 17:49:54 +0200

    Add more logic on the dumper

--- src/Infrastructure/Dumper/MuscleAndStrength/MuscleAndStrengthDumper.php
+++ src/Infrastructure/Dumper/MuscleAndStrength/MuscleAndStrengthDumper.php
@@ -15,7 +15,7 @@ final class MuscleAndStrengthDumper
 
     public function dump(OutputInterface $output): void
     {
-        $output->writeln("Dumping all exercises from www.muscleandstrength.com");
+        $output->writeln('Dumping all exercises from www.muscleandstrength.com');
         // $this->dumpMuscleExercises('abductors', $output);
         $this->dumpMuscleExercises('abs', $output);
 //        $this->dumpMuscleExercises('Adductors', $output);
@@ -42,34 +42,59 @@ final class MuscleAndStrengthDumper
 
     private function dumpMuscleExercises(string $muscle, OutputInterface $output): void
     {
-        $output->writeln("Dumping " . $muscle . '...');
+        $output->writeln('Dumping ' . $muscle . '...');
         try {
             $currentPageUrl = self::DEFAULT_URL . '/' . $muscle . '?page=0&ajax=1';
             $firstPageDom = $this->getPageContent($currentPageUrl);
+            $firstPageXPath = new DOMXpath($firstPageDom);
+            $lastPageNumber = $this->getLastPage($firstPageXPath) + 1;
 
-            $xpath = new DOMXpath($firstPageDom);
-            $exercises = $xpath->query('//div[@class = "cell small-12 bp600-6"]');
-
-            $lastPageNodes = $xpath->query('//a[@title = "Go to last page"]');
-            
-            $lastPageUrl = $lastPageNodes->item(0)->attributes->getNamedItem('href')->nodeValue;
-            $lastPageNumber = (int) explode("&page=", $lastPageUrl)[1];
-
-
+            for ($page = 0; $page < $lastPageNumber; ++$page) {
+                $currentPageXPath = new DOMXpath(
+                    $this->getPageContent(self::DEFAULT_URL . '/' . $muscle . '?page=' . $page . '&ajax=1'),
+                );
+                $exercises = $currentPageXPath->query('//div[@class = "cell small-12 bp600-6"]');
+                dump($page);
+                dump($exercises);
+            }
         } catch (Exception $e) {
             $output->writeln("Can't dump " . $muscle . ' !');
             $output->writeln($e->getMessage() . '\n');
         }
     }
 
+    /**
+     * @throws Exception
+     */
     private function getPageContent(string $url): DOMDocument
     {
-        /** @var {string view} $pageContent */
+        /** @var object $pageContent */
         $pageContent = json_decode(file_get_contents($url));
+        /** @var string $pageContentView */
+        $pageContentView = $pageContent->view;
+
+        if (empty($pageContentView)) {
+            throw new Exception(sprintf('PageContent[view] cannot be find from %s', $url));
+        }
 
         $dom = new DOMDocument();
-        $dom->loadHTML($pageContent->view);
+        $dom->loadHTML($pageContentView);
 
         return $dom;
+    }
+
+    /**
+     * @throws Exception
+     */
+    private function getLastPage(DOMXpath $xpath): int
+    {
+        try {
+            $lastPageNodes = $xpath->query('//a[@title = "Go to last page"]');
+            $lastPageUrl = $lastPageNodes->item(0)?->attributes?->getNamedItem('href')->nodeValue;
+
+            return (int) explode('&page=', $lastPageUrl)[1];
+        } catch (Exception $exception) {
+            throw new Exception("Can't retrieve last page number", 0, $exception);
+        }
     }
 }
